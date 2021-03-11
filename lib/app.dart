f937b18bf16b468cb55153d82c7a8969117f8b27
Emanuele <70652793+Progxy@users.noreply.github.com>
2021-03-11 14:58:44 +0100

    fix in app

--- lib/app.dart
+++ lib/app.dart
@@ -7,12 +7,21 @@ import 'package:fdmCreator/screens/feedback.dart';
 import 'package:fdmCreator/screens/home.dart';
 import 'package:fdmCreator/screens/utilizzo.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'authentication_service.dart';
+import 'firebaseProjectsManager.dart';
 
 class MyApp extends StatelessWidget {
+  initializeFirebase() async {
+    await FirebaseProjectsManager().connectFirebaseSecondary();
+    await FirebaseProjectsManager().connectFirebaseDefault();
+    await Firebase.initializeApp();
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -34,7 +43,19 @@ class MyApp extends StatelessWidget {
           primaryColor: Color.fromARGB(255, 24, 37, 102),
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Access(),
+        home: FutureBuilder(
+          future: initializeFirebase(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return ErrorPage();
+            }
+            if (snapshot.hasData) {
+              print("resultfirst : ${snapshot.data}");
+              return Access();
+            }
+            return CircularProgressIndicator();
+          },
+        ),
         routes: {
           Access.routeName: (context) => Access(),
           AccessIscritti.routeName: (context) => AccessIscritti(),
